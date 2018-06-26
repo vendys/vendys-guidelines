948794aa0533884e9eb13ebfea91d0c248866c69
James G. Kim <jgkim@jayg.org>
2018-06-26 17:25:53 +0900

    feat: Changelog 생성을 위한 `conventional-changelog` 프리셋 추가

---
+++ changelog/README.md
@@ -0,0 +1,3 @@
+# Changelog
+
+이 [`conventional-changelog`](https://github.com/conventional-changelog/conventional-changelog) 프리셋은 벤디스의 [커밋 메시지 가이드라인](https://github.com/vendys/vendys-guidelines/blob/master/commit-messages/README.md)을 따라 작성된 코드로부터 `CHANGELOG.md`를 생성하기 위해 사용된다.
---
+++ changelog/parser-opts.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  headerPattern: /^(\w*)(?:\((.*)\))?: (.*)$/,
+  headerCorrespondence: [
+    'type',
+    'scope',
+    'subject',
+  ],
+  noteKeywords: ['BREAKING CHANGE'],
+  revertPattern: /^revert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
+  revertCorrespondence: ['header', 'hash'],
+  issuePrefixes: [
+    '#',
+    'gh-',
+    'PROD-',
+  ],
+};
